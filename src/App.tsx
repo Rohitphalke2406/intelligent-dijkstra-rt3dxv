d219b34228b2cecfac947c159b1919c440ff3246
Rohitphalke2406 <81799042+Rohitphalke2406@users.noreply.github.com>
2024-04-09 21:23:29 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Canvas from './views/Canvas';
+
+function App() {
+  return (
+    <div className="App h-screen">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
